4e71e469672e15d6dcd8c6b0e0c29e3191c47080
yrix90 <Ghjcnjgfhjkm981>
2019-01-16 23:45:12 +0700

    add class

---
+++ countfromby.py
@@ -0,0 +1,19 @@
+class CountFromBy:
+    def __init__(self, v: int=0, i: int=1) -> None:
+        self.val = v
+        self.incr = i
+
+    def increase(self) -> None:
+        self.val += self.incr
+
+    def __repr__(self) -> str:
+        return str(self.val)
+
+
+h = CountFromBy()
+print(h.val)
+h.increase()
+h.increase()
+h.increase()
+h.increase()
+print(h)
