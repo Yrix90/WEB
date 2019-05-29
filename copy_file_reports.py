76437401b7070c3996a0dc8d9c20b84d8b10ca0f
Yrix90 <41316752+Yrix90@users.noreply.github.com>
2019-05-29 10:51:04 +0700

    users_add

--- copy_file_reports.py
+++ copy_file_reports.py
@@ -11,74 +11,28 @@ pc063 = '//172.27.2.63/c$/Offline_opera_reports/'
 pc066 = '//172.27.2.66/c$/Offline_opera_reports/'
 pc065 = '//172.27.2.65/c$/Offline_opera_reports/'
 list_for_pc = [pc061, pc062, pc063, pc065, pc066]
-list_for_litter = {'*departure_all*': 'Departures.pdf', '*arrchkinbyroom*': 'Arrival and checked in today.pdf', '*gibyroom*': 'Guest in house (by room).pdf', '*finopbalall*': 'Open balance (individuals).pdf'}
+list_for_litter = {'*departure_all*': 'Departures.pdf', '*arrchkinbyroom*': 'Arrival and checked in today.pdf',
+                   '*gibyroom*': 'Guest in house (by room).pdf', '*finopbalall*': 'Open balance (individuals).pdf'}
 
 tm = datetime.datetime.now()
 os.chdir(src)
 files = os.listdir(src)
 
-logging.basicConfig(filename='//172.27.1.8/d$/offline_reports/info.log', level=logging.INFO)
-logging.info('Дата исполнения: %s' % tm)
-# поиск файла Departures
-files_departure = [file for file in fnmatch.filter(files, '*departure_all*')]
-if files_departure:
-    files_departure = max(files_departure, key=os.path.getctime)
-    logging.info('Последний файл Departures: %s' % files_departure)
-else:
-    logging.info('Последний файл Departures не найден')
+logging.basicConfig(format=u'%(levelname)-8s [%(asctime)s] %(message)s', filename='//172.27.1.8/d$/offline_reports/info.log', level=logging.INFO)
+logging.info('Дата исполнения: %s\n' % tm)
 
-# поиск файла arrival and checked in today
-files_arrchkinbyroom = [file for file in fnmatch.filter(files, '*arrchkinbyroom*')]
-if files_arrchkinbyroom:
-    files_arrchkinbyroom = max(files_arrchkinbyroom, key=os.path.getctime)
-    logging.info('Последний файл arrival and checked in today: %s' % files_arrchkinbyroom)
-else:
-    logging.info('Последний файл arrival and checked in today не найден')
-
-# поиск файла Guest in house (by room)
-files_gibyroom = [file for file in fnmatch.filter(files, '*gibyroom*')]
-if files_gibyroom:
-    files_gibyroom = max(files_gibyroom, key=os.path.getctime)
-    logging.info('Последний файл Guest in house (by room): %s' % files_gibyroom)
-else:
-    logging.info('Последний файл Guest in house (by room) не найден')
-
-# поиск файла Open balance (individuals)
-files_finopbalall = [file for file in fnmatch.filter(files, '*finopbalall*')]
-if files_gibyroom:
-    files_finopbalall = max(files_finopbalall, key=os.path.getctime)
-    logging.info('Последний файл Open balance (individuals): %s' % files_finopbalall)
-else:
-    logging.info('Последний файл Open balance (individuals) не найден')
-
-files_del = [files_departure, files_arrchkinbyroom, files_finopbalall, files_gibyroom]
-
-for dst in list_for_pc:
-    if os.path.isfile(files_departure):
-        shutil.copy2(files_departure, dst + 'Departures.pdf', follow_symlinks=True)
-        logging.info('Файл %s скопирован в %s' % (files_departure, dst))
+for filter_file in list_for_litter.keys():
+    search_file = [file for file in fnmatch.filter(files, filter_file)]
+    if search_file:
+        search_file = max(search_file, key=os.path.getctime)
+        logging.info('Последний файл %s: %s\n' % (list_for_litter.get(filter_file), search_file))
+        if os.path.isfile(search_file):
+            for dst in list_for_pc:
+                shutil.copy2(search_file, dst + list_for_litter.get(filter_file), follow_symlinks=True)
+                logging.info('Файл %s скопирован в %s\n' % (search_file, dst))
+            os.remove(search_file)
+            logging.info('Файл %s удален успешно\n' % search_file)
+        else:
+            logging.info('Не является файлом\n')
     else:
-        logging.info('Файл не найден')
-    if os.path.isfile(files_arrchkinbyroom):
-        shutil.copy2(files_arrchkinbyroom, dst + 'Arrival and checked in today.pdf', follow_symlinks=True)
-        logging.info('Файл %s скопирован в %s' % (files_arrchkinbyroom, dst))
-    else:
-        logging.info('Файл не найден')
-    if os.path.isfile(files_gibyroom):
-        shutil.copy2(files_gibyroom, dst + 'Guest in house (by room).pdf', follow_symlinks=True)
-        logging.info('Файл %s скопирован в %s' % (files_gibyroom, dst))
-    else:
-        logging.info('Файл не найден')
-    if os.path.isfile(files_finopbalall):
-        shutil.copy2(files_finopbalall, dst + 'Open balance (individuals).pdf', follow_symlinks=True)
-        logging.info('Файл %s скопирован в %s' % (files_finopbalall, dst))
-    else:
-        logging.info('Файл не найден')
-
-logging.info('************Удаление файлов на сервере***********')
-for file in files_del:
-    if os.path.isfile(file):
-        os.remove(file)
-        logging.info('Файл %s удален успешно' % file)
-    else:
-        logging.info('Файл %s ненайден' % file)
+        logging.info('Последний файл %s не найден\n' % list_for_litter.get(filter_file))
--- test_3.py
+++ test_3.py
@@ -1,10 +1,10 @@
 import logging
 import datetime
 
-logging.basicConfig(filename='//172.27.1.8/d$/offline_reports/info.log', level=logging.INFO)
+logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', filename='//172.27.1.8/d$/offline_reports/info.log', level=logging.INFO)
 
 a = 3
 b = 6
 c = a + b
 tm = datetime.datetime.now()
-logging.info('test %s' % tm)
+logging.info('test !!!!!!!!!!\n')
